b797f00963351af9f4b6e67b05276ed4755433b3
walte774 <walte774@morris.umn.edu>
2019-09-04 00:10:02 -0500

    Fix shellcheck warnings and add clarifying comments

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-n=$1
+n=$1 #We're using the function NthPrime, so we'll call our input n.
 directory=$2
-tar -xf NthPrime.tgz -C $directory
-cd $directory/NthPrime
-gcc -o NthPrime main.c nth_prime.c
-./NthPrime $n
+tar -xf NthPrime.tgz -C "$directory" #We extract the archive to the input directory.
+cd "$directory"/NthPrime || exit #We enter the directory where the files were extracted to, or exit if for some reason that directory doesn't exist.
+gcc -o NthPrime main.c nth_prime.c #We compile and link the C code in one step.
+./NthPrime "$n" #We call the program we created using our input n.
