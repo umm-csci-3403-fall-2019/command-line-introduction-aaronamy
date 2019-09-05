54db68375131a74f9397159213a6f6a4b74f6b06
walte774 <walte774@morris.umn.edu>
2019-09-04 19:40:59 -0500

    Extract the archive into the scratch directory
    This makes test 4 pass. Test 5 also passes.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 
+archive=$1 #This is the compressed archive we're going to work with.
+directory=$2 #This is the scratch directory we're going to work in.
+tar -xvf "$archive" -C "$directory" #We uncompress and extract the input archive to the input directory.
