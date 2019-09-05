3963d73c04dfe1a71913b191dd8cae227ca9fb81
walte774 <walte774@morris.umn.edu>
2019-09-04 22:37:54 -0500

    Clean and compress the directory
    This makes the rest of the tests pass.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 archive=$1 #This is the compressed archive we're going to work with.
+archiveName=${archive%.*} #We'll use this to navigate to the directory that contains our extracted files.
 directory=$2 #This is the scratch directory we're going to work in.
-tar -xvf "$archive" -C "$directory" #We uncompress and extract the input archive to the input directory.
+tar -xf "$archive" -C "$directory" #We uncompress and extract the input archive to the input directory.
+home="$PWD" #We'll keep track of this in case we need to go back to this location later.
+cd "$directory"/"$archiveName"
+grep -lrIZ "DELETE ME!" . | xargs -0 rm -f --
+cd "$directory"
+tar -zcf "$home"/cleaned_"$archive" "$archiveName" #We compress the archive, renaming it and sending it to the appropriate place.
