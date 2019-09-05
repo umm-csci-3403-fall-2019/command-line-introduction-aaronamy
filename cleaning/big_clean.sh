46657bda0067ca6faa6e499d9039652c86fd2a29
walte774 <walte774@morris.umn.edu>
2019-09-04 22:42:26 -0500

    Fix shellcheck warnings

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -5,7 +5,7 @@ archiveName=${archive%.*} #We'll use this to navigate to the directory that cont
 directory=$2 #This is the scratch directory we're going to work in.
 tar -xf "$archive" -C "$directory" #We uncompress and extract the input archive to the input directory.
 home="$PWD" #We'll keep track of this in case we need to go back to this location later.
-cd "$directory"/"$archiveName"
+cd "$directory"/"$archiveName" || exit
 grep -lrIZ "DELETE ME!" . | xargs -0 rm -f --
-cd "$directory"
+cd "$directory" || exit
 tar -zcf "$home"/cleaned_"$archive" "$archiveName" #We compress the archive, renaming it and sending it to the appropriate place.
